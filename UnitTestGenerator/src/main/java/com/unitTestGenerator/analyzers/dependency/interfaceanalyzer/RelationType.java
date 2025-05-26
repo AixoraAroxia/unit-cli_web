27761908d36116afa857e6405f5be159d0da6a55
Alejandro subero <alejandrosubero.ar@gmail.com>
2025-05-26 11:03:12 -0400

    preper

--- UnitTestGenerator/src/main/java/com/unitTestGenerator/analyzers/dependency/interfaceanalyzer/RelationType.java
+++
@@ -1,9 +0,0 @@
-package com.unitTestGenerator.analyzers.dependency.interfaceanalyzer;
-
-import com.unitTestGenerator.ioc.anotations.Component;
-
-
-public enum RelationType {
-    EXTENDS,
-    IMPLEMENTS
-}
--- UnitTestGenerator/src/main/java/com/unitTestGenerator/ioc/anotations/EndebleIOC.java
+++
@@ -1,9 +0,0 @@
-package com.unitTestGenerator.ioc.anotations;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface EndebleIOC {
-    String value();
-}
